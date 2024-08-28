aaa363fd1db5f1b474b75bd7e30fe92ffa60fe9a
Healing-A-dev <122822596+Healing-A-dev@users.noreply.github.com>
2024-08-28 16:24:54 -0400

    Update export_copy.lua

--- export_copy.lua
+++ export_copy.lua
@@ -15,7 +15,7 @@ if #arg > 0 then
            
     -a --adv: Advacent mode. Lets you choose where every block is placed and the file extension for each individual block.
 
-    -u --update: Updates the Blocks to the latest version.
+    -u --update: Updates Blocks to the latest version.
         ]])
         os.exit()
     elseif arg[1] == "-s" or arg[1] == "--silent" then
