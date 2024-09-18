a4bbf24158b46ba40e321ecb7e49d3b7b95ea8da
Healing-A-dev <122822596+Healing-A-dev@users.noreply.github.com>
2024-09-18 01:07:10 -0400

    Update export_copy.lua

--- export_copy.lua
+++ export_copy.lua
@@ -20,7 +20,7 @@ local function getConfig()
     local lines = file:lines()
     local config = {}
     for line in lines do
-        local var,value = line:match("(%w+):(%w+)")
+        local var,value = line:match("(%w+)%s?=%s?(%w+)")
         print(var,value)
         _G[var] = value
     end
