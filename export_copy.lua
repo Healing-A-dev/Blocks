013d8fc73a32dcc0c1cbdeedb8efe54333339c26
Healing-A-dev <122822596+Healing-A-dev@users.noreply.github.com>
2024-09-18 01:15:26 -0400

    Update export_copy.lua

--- export_copy.lua
+++ export_copy.lua
@@ -165,7 +165,7 @@ if #arg > 0 then
 elseif #arg == 0 then
     getConfig()
     print([[Blocks:
-    Version:]].."\027[95m 0.01\027[0m"..[[
+    Version:]].."\027[95m"..__VERSION.."\027[0m"..[[
 
     Use 'blocks -h' or 'blocks --help' for help.
     
