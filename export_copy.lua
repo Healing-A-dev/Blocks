6352c549c1cc68d0b54770ede87abf415f049f51
Healing-A-dev <122822596+Healing-A-dev@users.noreply.github.com>
2024-09-18 09:44:06 -0400

    Update export_copy.lua

--- export_copy.lua
+++ export_copy.lua
@@ -23,7 +23,7 @@ local function getConfig()
     for line in lines do
         if currentLine > 1 and #line:gsub("%s+","") > 0 then
             line = line:gsub("^%s+",""):gsub("%s+$","")
-            local var,value = line:match("(^.+):%s?(.+)")
+            local var,value = line:match("(.+):%s?(.+)")
             print(var,value)
             _G[var] = value
         end
