64c5bbb1ec8e650decc47d21a256679110c13c00
Healing-A-dev <122822596+Healing-A-dev@users.noreply.github.com>
2024-09-23 10:58:20 -0400

    Update export_copy.lua

--- export_copy.lua
+++ export_copy.lua
@@ -132,12 +132,12 @@ if #arg > 0 then
         print([[
     usage: blocks <operation> [...]
     available operations:
-        -h --help:                      Displays this message.
-        -e --export:                    Runs the 'Blocks Export Tool'.
-        -se --silent_silent_export:     Runs the 'Blocks Export Tool' and prevents the process lines from being printed
-        -a --adv:                       Advacent mode. Lets you choose where every block is placed and the file extension for each individual block.
-        -u --update:                    Updates Blocks to the latest version.
-        -R --uninstall:                 Uninstall Blocks and remove all saved blocks.]])
+        -h --help:               Displays this message.
+        -e --export:             Runs the 'Blocks Export Tool'.
+        -se --silent_export:     Runs the 'Blocks Export Tool' and prevents the process lines from being printed
+        -a --adv:                Advacent mode. Lets you choose where every block is placed and the file extension for each individual block.
+        -u --update:             Updates Blocks to the latest version.
+        -R --uninstall:          Uninstall Blocks and remove all saved blocks.]])
         os.exit()
     elseif arg[1] == "--update" or arg[1] == "-u" then
         local update_path = "https://github.com/Healing-A-Dev/Blocks"
