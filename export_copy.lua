a3accac94a20c17622b43e28b14d2dd6bbbe1c26
Healing-A-dev <122822596+Healing-A-dev@users.noreply.github.com>
2024-12-26 00:14:48 -0700

    Update export_copy.lua

--- export_copy.lua
+++ export_copy.lua
@@ -130,8 +130,9 @@ local function cache(blockName)
         local lineStore = holdBlocks[blockName].contents("*l")
         file:write("\n- "..es(blockName)..":\n")
         for _,i in pairs(lineStore) do
-            file:write("    "..es(i).."\n"..es("endBlock").."\n")
+            file:write("    "..es(i).."\n")
         end
+        file:write(es("endBlock").."\n")
     end
     file:close()
 end
